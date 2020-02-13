55b37c1821e80b54917a36353022ff5912b94c33
Chris Rowles <cmrowles@pm.me>
2020-02-13 00:48:19 +0000

    Apply fixes from StyleCI

--- config/controllers.php
+++ config/controllers.php
@@ -4,8 +4,8 @@
 | Register application controllers       |
 ----------------------------------------*/
 $controllers = [
-    'home' => new \Versyx\Codepad\Frontend\Controllers\HomeController($app),
-    'frontend' => new \Versyx\Codepad\Frontend\Controllers\FrontendEditorController($app)
+    'home'     => new \Versyx\Codepad\Frontend\Controllers\HomeController($app),
+    'frontend' => new \Versyx\Codepad\Frontend\Controllers\FrontendEditorController($app),
 ];
 
 return extract($controllers);
--- src/Frontend/Controllers/HomeController.php
+++ src/Frontend/Controllers/HomeController.php
@@ -17,6 +17,7 @@ class HomeController extends Controller
     public function view(array $data = [])
     {
         $viewData = $this->viewData($data);
+
         return $this->render('home.twig', $viewData);
     }
 }
